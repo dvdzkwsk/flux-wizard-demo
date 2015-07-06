899901a40fb29f337b7434f79772b2cf61c3bb11
David Zukowski <zukoweb@gmail.com>
2015-07-06 13:46:07 -0400

    fix(example): re-implements dispatcher override

--- example/index.jsx
+++ example/index.jsx
@@ -3,6 +3,9 @@ import Halcyon from 'halcyon';
 import ExampleDispatcher from './dispatcher';
 import ExampleWizard from './wizard';
 
+// We can override the default Halcyon Dispatcher
+Halcyon.registerStoresWithDispatcher(ExampleDispatcher);
+
 class ExampleApp extends ReactComponent {
   constructor () {
     super();
