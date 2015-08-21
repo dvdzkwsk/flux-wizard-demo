e1de266476361db3acc9da592c924e3a290aab1a
David Zukowski <zukoweb@gmail.com>
2015-08-21 15:50:03 -0400

    docs(wizard): starts adding jsdocs to core wizard component

--- src/components/wizard.jsx
+++ src/components/wizard.jsx
@@ -17,6 +17,25 @@ export default class HalcyonWizard extends React.Component {
     super();
   }
 
+  /**
+  * Takes a map of action creators and binds them to the Redux dispatcher and
+  * then directly to the wizard class instance. This faciliates dispatching
+  * Halcyon-specific actions because callers no longer need to specify `this`
+  * as the first argument to specify the target instance.
+  * @example
+  * const halcyonActions = {
+  *   foo : function () { ... }
+  * };
+  *
+  * // instead of doing:
+  * this.props.dispatch(halcyonActions.foo(this, ...));
+  *
+  * // you can do:
+  * this.bindActionCreatorsToWizard(halcyonActions);
+  * this.foo(...);
+  * @param {object} actions - object that defines a map of functions where
+  * the key is the function name and the value is its definition.
+  */
   bindActionCreatorsToWizard (actions) {
     const boundActions = bindActionCreators(actions, this.props.dispatch);
 
@@ -38,7 +57,6 @@ export default class HalcyonWizard extends React.Component {
   componentWillMount () {
     this.bindActionCreatorsToWizard(WizardActions);
     this.createWizard();
-    this.forceUpdate();
   }
 
   componentDidUnmount () {
@@ -47,14 +65,14 @@ export default class HalcyonWizard extends React.Component {
 
   componentDidUpdate (prevProps) {
     const prevState = prevProps.halcyon.get(this);
-    const state     = this.getCurrentState();
+    const currState = this.getCurrentState();
 
     // If this hook is called immediately after the wizard was created (during
     // the first render cycle) then return early since nothing actually changed.
-    if (!state || !prevState) return;
+    if (!currState || !prevState) return;
 
     // Broadcast completed navigation event if step index has changed
-    if (prevState.get('currentStepIndex') !== state.get('currentStepIndex')) {
+    if (prevState.get('currentStepIndex') !== currState.get('currentStepIndex')) {
       // do something?
     }
   }
@@ -62,9 +80,12 @@ export default class HalcyonWizard extends React.Component {
   // ----------------------------------
   // Halcyon Life Cycle Definition
   // ----------------------------------
-  // Determines whether or not a navigation attempt should be allowed to
-  // continue. By default will always return true; to prevent a navigation
-  // attempt return [Boolean] false.
+  /**
+  * Determines whether or not a navigation attempt should be allowed to
+  * continue. By default will always return true; to prevent a navigation
+  * attempt return false.
+  * @returns {boolean} Whether or not to stop a navigation attempt.
+  */
   shouldWizardNavigate () {
     return true;
   }
@@ -72,61 +93,84 @@ export default class HalcyonWizard extends React.Component {
   // ----------------------------------
   // Internal Halcyon Methods
   // ----------------------------------
-  // Routes all internal navigation attempts (either from the navigation bar
-  // or directional arrows). Helps hook into wizard and step lifecycle methods
-  // to determine if the wizard should proceed with the navigation.
-  // ------------------------
-  // [Number] Target Step Index
+  /**
+  * Routes all internal navigation attempts. Helps hook into lifecycle methods
+  * to determine if the wizard should proceed with the navigation.
+  * @param {integer} Index of the target step.
+  */
   attemptToNavigateToIndex (idx) {
     if (this.shouldWizardNavigate() && this.refs.step.shouldStepLeave()) {
       this.navigateToIndex(idx);
     }
   }
 
-  // Updates the active step index of the wizard
-  // ------------------------
-  // [Number] Target Step Index
+  /**
+  * Updates the active step of the wizard to the target index. Invokes
+  * "stepWillLeave" life cycle method in HalcyonStep before the step is
+  * unmounted.
+  * @param {integer} Index of the target step.
+  */
   navigateToIndex (idx) {
-    // this.wizardWillNavigate(idx);
-    this.refs.step.stepWillLeave();
-
-    // navigate...
+    if (typeof this.refs.step.stepWillLeave === 'function') {
+      this.refs.step.stepWillLeave();
+    }
     this.changeWizardStep(idx);
   }
 
-  // Handler that receives change events from the Navigation component.
-  // ------------------------
-  // [Number] Target Step Index
+  /**
+  * Handler that receives change events from navigation components. Helps
+  * to separate concerns between a requested navigation change and any
+  * actual action taken within the wizard.
+  * @param {integer} Index of the target step.
+  */
   onNavigationChange (idx) {
     this.attemptToNavigateToIndex(idx);
   }
 
   // ----------------------------------
-  // Wizard State Helpers
+  // State Convenience Methods
   // ----------------------------------
+  /**
+  * @returns {Map|Undefined} State of the current wizard instance.
+  */
   getCurrentState () {
-    const store = this.props.halcyon;
+    const instances = this.props.halcyon;
 
-    return store && store.get(this);
+    return instances && instances.get(this);
   }
 
+  /**
+  * @returns {array} Collection of direct child steps.
+  */
   getSteps () {
     return this.props.children.length ?
       this.props.children : [this.props.children];
   }
 
+  /**
+  * @returns {number} The index of the active step.
+  */
   getCurrentStepIndex () {
     return this.getCurrentState().get('currentStepIndex');
   }
 
+  /**
+  * @returns {boolean} True iff the wizard is on the first step.
+  */
   isOnFirstStep () {
     return this.getCurrentStepIndex() === 0;
   }
 
+  /**
+  * @returns {boolean} True iff the wizard is on the last step.
+  */
   isOnLastStep () {
     return this.getCurrentStepIndex() === this.getSteps().length - 1;
   }
 
+  /**
+  * @returns {boolean} Whether or not the wizard is in a disabled state.
+  */
   isDisabled () {
     return false;
   }
